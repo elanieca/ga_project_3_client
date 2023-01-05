b470d7028c479bb7a68fb0edac77e874a7329231
elanieca <elanieca@hotmail.com>
2023-01-05 17:29:05 +0100

    basic file structure; added navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navigation/Navbar';
 
 window.Buffer = window.Buffer || require('buffer').Buffer;
 
+// Navigation -> Navbar, NavDrawer, Avatar
+// "/welcome" -> Welcome
+// "/register" -> Register
+// "/login" -> Login
+// "/" -> LatestPosts -> PostCard
+// "/diary-entries" -> DiaryIndex -> BookCard
+// "/diary-entries/:id" -> BookShow
+// "/dashboard/my-library" -> Dashboard -> UserBooks
+// "/dashboard/favourites" -> Dashboard -> FavoriteBooks
+// "/dashboard/add-book" -> Dashboard -> AddBook
+
 const App = () => (
   <Router>
+    <Navbar />
     <Routes>
       <Route path="/" element={<p>React App</p>} />
     </Routes>
